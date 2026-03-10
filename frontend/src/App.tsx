5356b55ce703edb1d52f56820578eab65a696939
Paulo Henrique Silva <paulo@imdigital.com>
2026-03-10 18:43:40 -0300

    Create Vite React App. Import Tailwind Css

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50">
+      <h1 className="text-3xl font-bold text-gray-900">Dashboard</h1>
+    </div>
+  )
+}
+
+export default App
